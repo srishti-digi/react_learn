fdd18487e8ea66be3e8e407f7914d59965f2f471
srishti-digi <smishra@digisprint.com>
2024-05-09 12:54:18 +0530

    fixed props

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hello from './components/Hello';
 function App() {
  return <div className="App">
   <Hello message="Hii"   name="Srishti"/>
+  <Hello message="Hello"   name="People"/>
       </div>
 }
 
