dc583debd458ac16b619ab1f1f4d6bcbd21170e0
srishti-digi <smishra@digisprint.com>
2024-05-09 12:18:30 +0530

    jsx created

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,5 +1,11 @@
+
 function Hello(){
-    return <h1>Hello from component</h1>
+   
+    return (
+    <div><h1>Hello from component</h1><h1>Hello people</h1>
+
+    </div>
+    );
 }
 
 export default Hello
